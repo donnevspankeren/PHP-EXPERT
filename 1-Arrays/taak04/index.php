ab7b4f39ea3bd0062a425c1a685b88f855cbd9b0
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-09-02 10:55:48 +0200

    taak 4 array

--- 1-Arrays/taak04/index.php
+++ 1-Arrays/taak04/index.php
@@ -18,7 +18,9 @@ $speelgoed = [
         'prijs' => '39.00'
     ],
 ];
-
+echo "Naam van het speelgoed is: " . $speelgoed[0]["naam"]." ,Het soort speelgoed is: ". $speelgoed[0]["soort"]." en de prijs is: ". $speelgoed[0]["prijs"] . "<br>";
+echo "Naam van het speelgoed is: " . $speelgoed[1]["naam"]." ,Het soort speelgoed is: ". $speelgoed[1]["soort"]." en de prijs is: ". $speelgoed[1]["prijs"] . "<br>";
+echo "Naam van het speelgoed is: " . $speelgoed[2]["naam"]." ,Het soort speelgoed is: ". $speelgoed[2]["soort"]." en de prijs is: ". $speelgoed[2]["prijs"] . "<br>";
 
 $games = [
     'sport' => [
@@ -37,3 +39,7 @@ $games = [
         'prijs' => '40.00'
     ]
 ];
+
+echo "De naam van de game is: " . $games["sport"]["naam"] . ", De uitgever is: " . $games["sport"]["uitgever"] . " en de prijs van het spel is: " . $games["sport"]["prijs"]."<br>";
+echo "De naam van de game is: " . $games["action"]["naam"] . ", De uitgever is: " . $games["action"]["uitgever"] . " en de prijs van het spel is: " . $games["action"]["prijs"]."<br>";
+echo "De naam van de game is: " . $games["fps"]["naam"] . ", De uitgever is: " . $games["fps"]["uitgever"] . " en de prijs van het spel is: " . $games["fps"]["prijs"];
