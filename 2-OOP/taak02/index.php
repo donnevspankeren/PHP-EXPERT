0f0c3b18a57efe3a06c5614cfa586f9c84651b06
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-09-21 13:44:13 +0200

    Taak 2 OOP

--- 2-OOP/taak02/index.php
+++ 2-OOP/taak02/index.php
@@ -1,11 +1,11 @@
 <?php
 
 class Auto{
-$merk;
-$kleur;
-$type;
-$uitvoering;
-$brandstof;
+public $merk;
+public $kleur;
+public $type;
+public $uitvoering;
+public $brandstof;
 }
 
 $nieuweAuto = new Auto();
@@ -15,7 +15,11 @@ $nieuweAuto->type = "Model 3";
 $nieuweAuto->uitvoering = "cabrio";
 $nieuweAuto->brandstof = "elektrisch";
 
-echo $nieuweAuto->merk;
+echo $nieuweAuto->merk. "<br>";
+echo $nieuweAuto->kleur. "<br>";
+echo $nieuweAuto->type. "<br>";
+echo $nieuweAuto->uitvoering. "<br>";
+echo $nieuweAuto->brandstof. "<br>";
 $nogEenAndereAuto = new Auto();
 
 
