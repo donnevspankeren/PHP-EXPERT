207437f1ca3f88bdd4e9d70ef31deb0b26e68f52
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-09-21 13:50:06 +0200

    taak3 OOP

---
+++ 2-OOP/taak03/index.php
@@ -0,0 +1,22 @@
+<?php
+
+class Product{
+    public $merk;
+    public $prijs;
+    public $type;
+    public $kleur;
+}
+
+$dweil = new Product();
+$dweil->merk="Hema";
+$dweil->prijs=10;
+$dweil->type="lang";
+$dweil->kleur="rood";
+
+$Stofzuiger = new Product();
+$Stofzuiger->merk="eagon";
+$Stofzuiger->prijs=50;
+$Stofzuiger->type="gfs505";
+$Stofzuiger->kleur="grijs";
+
+?>
