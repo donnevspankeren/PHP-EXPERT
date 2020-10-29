aa3e395ac5d74757ddf6edec4f999b08ce250b28
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-10-29 14:24:15 +0100

    taak 3 PDO select

--- 3-PDO-SELECT/taak03/index.php
+++ 3-PDO-SELECT/taak03/index.php
@@ -7,12 +7,15 @@ $database_wachtwoord  = '';
 
 $database_connectie = new PDO("mysql:host=$database_lokatie;dbname=$database_naam", $database_gebruiker, $database_wachtwoord);
 
+$firstname = "Mohamed";
+
 $sql = 'SELECT * FROM users WHERE firstname = :ph_firstname';
 $statement = $database_connectie->prepare($sql);
 $statement->bindParam(":ph_firstname", $firstname);
-$user = $statement->fetch();
- 
-foreach ($user as $value) {
-    echo $value['ph_firstname'] . "<br>";
+$statement->execute();
+$user = $statement->fetch(PDO::FETCH_ASSOC); //fetch gebruikt omdat maar 1 rij geselecteerd is. ANders fetchAll()
+//  var_dump($user);
+foreach ($user as $key => $value) {
+    echo $key . ":" . $value . "<br>";
 }
 ?>
