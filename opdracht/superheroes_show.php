ee13d28f0653decea7ee83826036b284002be2d3
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-11-02 10:15:11 +0100

    show pagina

--- opdracht/superheroes_show.php
+++ opdracht/superheroes_show.php
@@ -2,33 +2,24 @@
 require 'database.php';
 include 'header.php';
 
-$id=$_GET['ID'];
+$id=$_GET['id'];
 
 $sql = "SELECT * FROM superheroes WHERE ID = :ph_id";
 $statement = $db_conn->prepare($sql);
-$statement->bindParam(":ph_id", $ph_id);
+$statement->bindParam(":ph_id", $id);
 $statement->execute();
 $database_gegevens = $statement->fetch(PDO::FETCH_ASSOC);
 ?>
+<a href="superheroes_index.php">TERUG!</a>
 <table class="table">
-<thead>
-    <tr>
-        <th>ID</th>
-        <th>Title</th>
-        <th>Alignment</th>
-        <th></th>
-    </tr>
-</thead>
 <tbody>
-    <?php foreach($database_gegevens as $hero):?>
+    <?php foreach($database_gegevens as $key => $hero):?>
         <tr>
-            <td><?php echo $hero['ID']?></td>
-            <td><?php echo $hero['Title']?></td>
-            <td><?php echo $hero['Alignment']?></td>
-        </tr>
+            <td><?php echo $key . ": " . $hero ?></td>
         </tr>
     <?php endforeach;?>
 </tbody>
 </table>
+
 
 
